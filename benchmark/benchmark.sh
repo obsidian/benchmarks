f31b6e2c1673462ee6321cb3ba9b251ffe25ecb5
jwaldrip <jason@waldrip.net>
2017-12-12 12:30:12 -0700

    update benchmarks

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-echo "Running benchmarks for '$1'"
-touch $OUT_FILE
-echo "Test results for '$1':" | tee -a $OUT_FILE
-bombardier -c 100 -d 30s http://$1:${PORT}${TEST_ROUTE} | tee -a $OUT_FILE
+echo "----------------------------------------------"  | tee -a $OUT_FILE
+echo "Benchmarks for $1"  | tee -a $OUT_FILE
+echo "----------------------------------------------"  | tee -a $OUT_FILE
+hey -c 100 -n 100000 http://$1:${PORT}${TEST_ROUTE} | tee -a $OUT_FILE
 echo "" | tee -a $OUT_FILE
--- http/app.cr
+++ http/app.cr
@@ -1,4 +1,5 @@
 require "http"
 
 HTTP::Server.new(port: ENV["PORT"].to_i, host: "0.0.0.0") do |c|
+  c.response.close
 end.listen
