7a405d2dd6633adf76507541bbbd729d825d2185
jwaldrip <jason@waldrip.net>
2018-01-08 23:52:58 -0700

    delay bench 1s

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -3,7 +3,7 @@ set -e
 echo "----------------------------------------------"  | tee -a $OUT_FILE
 echo "Benchmarks for $1"  | tee -a $OUT_FILE
 echo "----------------------------------------------"  | tee -a $OUT_FILE
-sleep 10
+sleep 1
 curl http://$1:${PORT}${TEST_ROUTE}
 hey -c 100 -n 100000 http://$1:${PORT}${TEST_ROUTE} | tee -a $OUT_FILE
 echo "" | tee -a $OUT_FILE
